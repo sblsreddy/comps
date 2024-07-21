f6f1c8cca47be4bc84aebfbcbff950e83f5c1937
sblsreddy <r14141@gmail.com>
2024-07-21 07:31:07 -0500

    Parent Child relationship between elements and place holder. usage of custom directive

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceHolderComponent } from './place-holder/place-holder.component';
+import { TimesDirective } from './times.directive';
 
 
 @NgModule({
   declarations: [
     ElementsHomeComponent,
-    PlaceHolderComponent
+    PlaceHolderComponent,
+    TimesDirective
   ],
   imports: [
     CommonModule,
